dfb144abdb1c829efa00d96648b59610828b838d
Hardworking-tester <373391120@qq.com>
2017-02-18 13:51:30 +0800

    hahah

---
+++ practice/zhengze.py
@@ -0,0 +1,9 @@
+# encoding:utf-8
+# author:wwg
+from selenium import webdriver
+
+for i in range (5000,9999):
+    print i
+# br=webdriver.Firefox()
+# br.get("http://www.baidu.com")
+# print "wwwg"
