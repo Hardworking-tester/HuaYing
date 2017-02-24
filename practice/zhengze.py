b9179a8dab5190e2c18c6dcb3b0f6c0b69943322
Hardworking-tester <373391120@qq.com>
2017-02-24 16:58:11 +0800

    hahah

--- practice/zhengze.py
+++ practice/zhengze.py
@@ -1,10 +1,69 @@
 # encoding:utf-8
 # author:wwg
-from selenium import webdriver
+import httplib
+import urllib
+import json
+import urllib2
+import re
+import os
 
-for i in range (5000,9999):
-    print i
-# br=webdriver.Firefox()
-# br.get("http://www.baidu.com")
-# print "wwwg"
-print "wwg2"
+
+class BaiduImage(object):
+    def __init__(self):
+        super(BaiduImage, self).__init__()
+        print u'图片获取中,CTRL+C 退出程序...'
+        self.page = 30  # 当前页数
+        if not os.path.exists(r'F:\testresult\image'):
+            os.mkdir(r'F:\testresult\image')
+
+    def request(self):
+        try:
+            while 1:
+
+                conn = httplib.HTTPConnection('image.baidu.com')
+                # request_url = '/search/avatarjson?tn=resultjsonavatarnew&ie=utf-8&word=%E7%BE%8E%E5%A5%B3&cg=girl&rn=60&pn=' + str(
+                #     self.page)
+                request_url = 'http://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord=%E7%BE%8E%E5%A5%B3&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&word=%E7%BE%8E%E5%A5%B3&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&cg=girl&pn=' + str(
+                    self.page) + '&rn=30&gsm=1e00000000001e&1487920679638='
+                headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0',
+                           'Content-type': 'test/html'}
+                # body = urllib.urlencode({'tn':'resultjsonavatarnew','ie':'utf-8','word':'%E7%BE%8E%E5%A5%B3','cg':'girl','pn':self.page,'rn':'60'})
+                conn.request('GET', request_url, headers=headers)
+                r = conn.getresponse()
+                # print r.status
+                if r.status == 200:
+                    data = r.read()
+                    print "data is : %s" %data
+                    data = unicode(data, errors='ignore')
+                    decode = json.loads(data)
+                    print "decode is : %s" %decode
+                    self.download(decode['data'])
+
+                self.page += 30
+        except Exception, e:
+            print e
+        finally:
+            conn.close()
+
+    def download(self, data):
+        print "data2 is : %s" %data
+        for d in data:
+            # url = d['thumbURL']   缩略图  尺寸200
+            # url = d['hoverURL']           尺寸360
+            print d
+            if d:
+                # print "-----------------"+str(d)+"----------------------------"
+                url = d['hoverURL']
+                data = urllib2.urlopen(url).read()
+
+                pattern = re.compile(r'.*/(.*?)\.jpg', re.S)
+                item = re.findall(pattern, url)
+                FileName = str(r'F:\testresult\image') + item[0] + str('.jpg')
+
+                with open(FileName, 'wb') as f:
+                    f.write(data)
+
+
+if __name__ == '__main__':
+    bi = BaiduImage()
+    bi.request()
